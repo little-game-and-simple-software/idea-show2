e672a4a80a09e78b5637bdc33a51528f34115896
2439905184 <suchenglong018@126.com>
2022-09-30 16:23:24 +0800

    🌈 style: 美化代码

--- load.js
+++ load.js
@@ -2,9 +2,7 @@ console.log("hello world")
 //从github读取txt文件
 function my_load()
 {
-      var user_ideas2=document.createElement("p")
-
- 
+    var user_ideas2=document.createElement("p")
     var request=new XMLHttpRequest()
     //输出
     var out
@@ -12,13 +10,13 @@ function my_load()
    // var url="https://raw.githubusercontent.com/little-game-and-simple-software/idea-show/gh-pages/ideas.txt//"
     //"ideas.txt"
     var op=request.open("get","ideas.txt",true)
-   request.onload=function()
-   {
-       console.log(request.status)
-       console.log("状态"+request.status)
-      // document.write("状态"+request.status)
-       console.log(request.responseText)
-       out=request.responseText
+    request.onload=function()
+    {
+        console.log(request.status)
+        console.log("状态"+request.status)
+        // document.write("状态"+request.status)
+        console.log(request.responseText)
+        out=request.responseText
         if (request.readyState===request.DONE)
         {
             if(request.status===200)
@@ -28,18 +26,14 @@ function my_load()
             }
             
         }
- 
-   }
-request.send() 
- var user_ideas=document.createElement("p")
-   console.log(user_ideas)
-   user_ideas.innerHTML="新文本!"
-  
-   
-      document.body.appendChild(user_ideas2)
-  // var body=document.getElementByName("body");
-   document.body.appendChild(user_ideas)
-   console.log(typeof(request.responseText))
-   
+    }
+    request.send() 
+    var user_ideas=document.createElement("p")
+    console.log(user_ideas)
+    user_ideas.innerHTML="新文本!"
+    document.body.appendChild(user_ideas2)
+    // var body=document.getElementByName("body");
+    document.body.appendChild(user_ideas)
+    console.log(typeof(request.responseText))
 }
 my_load()
