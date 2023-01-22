55d6f11825a27c96b549ce366ca337de69a1ed47
13023335265 <suchenglong018@126.com>
2023-01-22 20:12:35 +0800

    add

---
+++ 学历提升区/adder.js
@@ -0,0 +1,37 @@
+function add_root_list()
+{
+    var ol = document.createElement("ol")
+}
+add_root_list()
+add_a_item()
+
+function add_a_item()
+{
+    var li = document.createElement("li")
+    var option = create_option()
+    
+    function create_option()
+    {
+        // p_detail: type:json data {}
+        function add_selection(p_detail:{})
+        {
+            function add_A(p_content)
+            {
+
+            }
+            function add_B(p_content)
+            {
+
+            }
+            function add_C(p_content)
+            {
+
+            }
+            function add_D(p_content)
+            {
+
+            }
+        }
+
+    }
+}
---
+++ 学历提升区/li.vue
@@ -0,0 +1,5 @@
+<li> {{title}}......</li> 
+    <input type="radio" v-bind:id="value"/>  {{optionA}}
+    <input type="radio" v-bind:id="value"/>  {{optionB}}
+    <input type="radio" v-bind:id="value"/>  {{optionC}}
+    <input type="radio" v-bind:id="value"/>  {{optionD}}
