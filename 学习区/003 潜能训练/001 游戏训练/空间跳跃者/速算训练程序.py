06b65a46f15067d1c1080dec4c6ec6b67d86c85b
13023335265 <suchenglong018@126.com>
2023-04-12 13:19:03 +0800

    补充

--- 学习区/003 潜能训练/001 游戏训练/空间跳跃者/速算训练程序.py
+++ 学习区/003 潜能训练/001 游戏训练/空间跳跃者/速算训练程序.py
@@ -1,14 +1,22 @@
-# 伪代码，需要整理
+
+import random
 while True:
-	x1 = random(0,10)
-	y1 = random(0,10)
-	z1 = random(0,10)
+	x1 = random.randint(0,10)
+	y1 = random.randint(0,10)
+	z1 = random.randint(0,10)
 	
-	x2 = random(0,10)
-	y2 = random(0,10)
-	z2 = random(0,10)
+	x2 = random.randint(0,10)
+	y2 = random.randint(0,10)
+	z2 = random.randint(0,10)
 	
-	print("answer:")
 	vec3_1 = [x1,y1,z1]
 	vec3_2 = [x2,y2,z2]
-	print(vec3_1+vec3_2)
+	print(vec3_1)
+	print(vec3_2)
+	pre_calcuate = vec3_1 + vec3_2
+	print(pre_calcuate)
+	result = input("请输入计算结果")
+	if result == pre_calcuate:
+		print("计算成功")
+	else:
+		print("计算失败")
