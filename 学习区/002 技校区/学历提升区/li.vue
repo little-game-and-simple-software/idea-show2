fe228c3716fe5f441c2c84c1a5c1a31e741c2a63
13023335265 <suchenglong018@126.com>
2023-04-18 17:09:05 +0800

    : clean:剥离网站结构，重组
    把自由学习区划分到另外一个项目

--- 学习区/002 技校区/学历提升区/li.vue
+++
@@ -1,5 +0,0 @@
-<li> {{title}}......</li> 
-    <input type="radio" v-bind:id="value"/>  {{optionA}}
-    <input type="radio" v-bind:id="value"/>  {{optionB}}
-    <input type="radio" v-bind:id="value"/>  {{optionC}}
-    <input type="radio" v-bind:id="value"/>  {{optionD}}
